c778762e98e02b6018f946bbbca496e1f858875d
Jerrad <Jerrad.stutzman@gmail.com>
2017-05-29 17:55:18 -0700

    Add files via upload

---
+++ webAssignment4/four.js
@@ -0,0 +1,10 @@
+document.body.style.backgroundColor = "red";
+
+document.body.style.fontFamily = "sans-serif";
+
+document.querySelector("h1").style.backgroundColor = "blue";
+
+document.getElementById("nickname").innerHTML = "Stuey"
+document.getElementById("favorites").innerHTML = "Coding"
+document.getElementById("hometown").innerHTML = "Grand Island"
+
